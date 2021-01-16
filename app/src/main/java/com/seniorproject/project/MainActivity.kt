39f497673c8434ca0562d7e743909931f4e565d8
tatsu <nassavakasem@gmail.com>
2021-01-16 16:11:36 +0700

    LAST TEST

--- app/src/main/java/com/seniorproject/project/MainActivity.kt
+++ app/src/main/java/com/seniorproject/project/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 //      //HELLO BITCHES
-        //FUCK U INDIAN
+
+        //HELLO NEW
     }
 }
