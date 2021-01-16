2c69c52aa3d93585260c20a76bed50d1bb7e0c96
tatsu <nassavakasem@gmail.com>
2021-01-16 16:15:05 +0700

    Final version test done

--- app/src/main/java/com/seniorproject/project/MainActivity.kt
+++ app/src/main/java/com/seniorproject/project/MainActivity.kt
@@ -14,8 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-//test
-        //Hello
+
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -23,8 +22,6 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-//      //HELLO BITCHES
 
-        //HELLO NEW
     }
 }
