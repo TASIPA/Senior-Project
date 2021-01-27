aba72b61789a9797ec8987449f7b46c05471c6fd
jerry <Sirendra99@gmail.com>
2021-01-27 22:05:23 +0700

    test

--- app/src/main/java/com/seniorproject/project/LoginActivity.kt
+++ app/src/main/java/com/seniorproject/project/LoginActivity.kt
@@ -2,10 +2,13 @@ package com.seniorproject.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        supportActionBar!!.hide()
+
     }
 }
