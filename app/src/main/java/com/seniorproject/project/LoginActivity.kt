e3404a55107aa5e326e20818e7cd968560634b60
tatsu <nassavakasem@gmail.com>
2021-01-30 11:18:45 +0700

    Add Sign In and Sign Up Authenthication feature

--- app/src/main/java/com/seniorproject/project/LoginActivity.kt
+++ app/src/main/java/com/seniorproject/project/LoginActivity.kt
@@ -1,19 +1,71 @@
 package com.seniorproject.project
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.ImageView
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth //Declare the connection to firebase authentication
+    private lateinit var dialog: ProgressDialog
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar!!.hide()
-        registerBtn.setOnClickListener {
-            var intent = Intent(this, SignupActivity::class.java)
+
+        auth = FirebaseAuth.getInstance()
+        dialog = ProgressDialog(this)
+
+        LoginBtn.setOnClickListener {
+            login()
+        }
+        registerBtn.setOnClickListener {//change to signup page once user clicks
+            val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
     }
+
+    private fun login() { //Login page
+        var email = emailText.text.toString() //put the input data to email variable
+        var pass = passText.text.toString() //put the input data to pass variable
+
+        //check the possible conditions of user errors
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailText.error = "Please enter a VALID Email format"
+            return
+        } else if (pass.length < 5) {
+            passText.error = "Password should be greater than 5 characters"
+            return
+        } else if (pass.isEmpty()) {
+            passText.error = "Please enter a password!"
+            return
+        } else {
+            //when it is not error, it will set dialog once user press log in while comparing user's data
+            dialog.setMessage("Please Wait..")
+            dialog.show()
+            dialog.setCanceledOnTouchOutside(false)
+            auth.signInWithEmailAndPassword(email, pass)//pass value to firebase authentication
+                .addOnCompleteListener(this, OnCompleteListener { task ->
+                    if (task.isSuccessful) {//check if it contains this user in the authentication data
+                        dialog.cancel()
+                        Toast.makeText(this, "Successfully Login", Toast.LENGTH_LONG).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)//move to the restaurant interface : our kt name not matched!
+                        finish()
+                    } else {
+                        dialog.cancel()
+                        Toast.makeText(this, "Failed to Login", Toast.LENGTH_LONG).show()
+                    }
+                })
+        }
+    }
 }
+
--- app/src/main/java/com/seniorproject/project/SignupActivity.kt
+++ app/src/main/java/com/seniorproject/project/SignupActivity.kt
@@ -1,22 +1,89 @@
 package com.seniorproject.project
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var dialog:ProgressDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        auth= FirebaseAuth.getInstance()
+        dialog= ProgressDialog(this)
         supportActionBar!!.hide()
         nextBtn.setOnClickListener {
             first.visibility=INVISIBLE
             second.visibility= VISIBLE
         }
+
         SignupBtn.setOnClickListener {
-            finish()
+            register()
         }
+    }
+    private fun register() {
+        var email = emailSignupText.text.toString() //store the input
+        var pass = passSignupText.text.toString() //store the input
+        var cpass = repassSignupText.text.toString() //store the input
+
+        //check the possible condition of user errors
+        if (email.isEmpty()) {
+            emailSignupText.error = "Please enter your Email"
+            return
+        }
+        if (pass.isEmpty()) {
+            passSignupText.error = "Please enter a password"
+            return}
+        if (cpass.isEmpty()) {
+            repassSignupText.error = "Please confirm your password"
+            return
+        }
+        if (cpass != pass) {
+            Toast.makeText(this, "Password is Not Matched", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailSignupText.error = "Invalid Email Format"
+            return
+        }
+        if (pass.length < 5) {
+            passSignupText.error = "Password should be greater than 5 Characters"
+            return
+        }
+
+        //check whether the checkBox is tick or not, if not user won't be able to register
+
+        if(!checkBox.isChecked){
+            Toast.makeText(this, "Agree & Read TOS", Toast.LENGTH_SHORT).show()
+            return
+        }
+        //set the dialog running to the user when it creating account to the firebase authentication
+
+        dialog.setMessage("..Creating Account..")
+        dialog.show()
+        dialog.setCanceledOnTouchOutside(true)
+
+        auth.createUserWithEmailAndPassword(email, pass) //function to create the account by passing the value that we collected
+            .addOnCompleteListener(this, OnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    dialog.cancel()//stop the dialog running when created account in the authentication successfully
+                    Toast.makeText(this, "Account Created", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, LoginActivity::class.java)//back to the login page
+                    startActivity(intent)
+                    finish()
+                } else {
+                    dialog.cancel()
+                    Toast.makeText(this, "Failed to create Account", Toast.LENGTH_LONG).show()
+                }
+            })
     }
 }
