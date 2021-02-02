34c4c39811629cc9b22c56b4c7066d95a77cc139
Peerayos Lokwipas <paint.yb@gmail.com>
2021-02-02 22:46:23 +0700

    Spinner added (havent decorate)

--- app/src/main/java/com/seniorproject/project/ReportActivity.kt
+++ app/src/main/java/com/seniorproject/project/ReportActivity.kt
@@ -3,19 +3,42 @@ package com.seniorproject.project
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_report.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-
 class ReportActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report)
+        // access the items of the list
+        val languages = resources.getStringArray(R.array.Topics)
 
-        val current = LocalDateTime.now()
-        val formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm ")
-        val formatted = current.format(formatter)
+        // access the spinner
+        val spinner = findViewById<Spinner>(R.id.topicReport)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                    android.R.layout.simple_spinner_item, languages)
+            spinner.adapter = adapter
 
-        dateShowReport.text = "Date: $formatted"
+            spinner.onItemSelectedListener = object :
+                    AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                 /*   Toast.makeText(this@ReportActivity,
+                            getString(R.string.selected_item) + " " +
+                                    "" + languages[position], Toast.LENGTH_SHORT).show()*/
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
     }
 }
