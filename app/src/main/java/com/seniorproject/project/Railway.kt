fb5daff0b594e64d19641e367e170c6d3e210b19
ttatsutat_macbook <nassavakasem@gmail.com>
2021-02-12 00:36:17 +0700

    Railway Webview link added!

--- app/src/main/java/com/seniorproject/project/Railway.kt
+++ app/src/main/java/com/seniorproject/project/Railway.kt
@@ -1,11 +1,19 @@
 package com.seniorproject.project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_railway.*
 
 class Railway : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_railway)
+
+        webBTN.setOnClickListener {
+            val intent = Intent(this, WebActivity::class.java)//back to the login page
+            startActivity(intent)
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/seniorproject/project/WebActivity.kt
@@ -0,0 +1,25 @@
+package com.seniorproject.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class WebActivity : AppCompatActivity() {
+    private lateinit var Webview: WebView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web)
+        Webview = findViewById(R.id.webVieww)
+        Webview.settings.setJavaScriptEnabled(true)
+        Webview.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+        Webview.loadUrl("https://www.google.com")
+    }
+}
