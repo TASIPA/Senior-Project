d3a110f28f63bbef4bc2794194fbfa7104d00de5
Peerayos Lokwipas <paint.yb@gmail.com>
2021-03-01 20:05:52 +0700

    All detail pages

--- app/src/main/java/com/seniorproject/project/ReportActivity.kt
+++ app/src/main/java/com/seniorproject/project/ReportActivity.kt
@@ -1,6 +1,5 @@
 package com.seniorproject.project
 
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/com/seniorproject/project/ResDetailActivity.kt
@@ -0,0 +1,57 @@
+package com.seniorproject.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class ResDetailActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var latLng: LatLng
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_res_detail)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        val bundle = intent.extras
+        var pointLat = bundle?.getString("lati").toString().toDouble()
+        var pointLon = bundle?.getString("longi").toString().toDouble()
+        //use to calculate distance to User
+        var name = bundle?.getString("NAME").toString()
+        var type = bundle?.getString("TYPE").toString()
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val bundle = intent.extras
+        var pointLat = bundle?.getString("lati").toString().toDouble()
+        var pointLon = bundle?.getString("longi").toString().toDouble()
+        var name = bundle?.getString("NAME").toString()
+        latLng = LatLng(pointLat,pointLon)
+        // Add a marker in Sydney and move the camera
+        mMap.addMarker(MarkerOptions().position(latLng).title(name))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+        if (name==null) {
+            finish()
+        }
+    }
+}
