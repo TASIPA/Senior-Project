d33040354f6a715dcb766c3a880172b0fb6a7119
jerry <Sirendra99@gmail.com>
2021-05-08 18:44:56 +0700

    Made everypage accessible through categories

--- app/src/main/java/com/seniorproject/project/Allcategories.kt
+++ app/src/main/java/com/seniorproject/project/Allcategories.kt
@@ -1,7 +1,10 @@
 package com.seniorproject.project
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_category.*
 
 class Allcategories : AppCompatActivity() {
 
@@ -9,4 +12,42 @@ class Allcategories : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
         supportActionBar!!.hide()
+        back_btn.setOnClickListener {
+            finish()
+        }
+        cat_res.setOnClickListener {
+            var intent= Intent(this,RestaurantActivity::class.java)
+            startActivity(intent)
+
+        }
+        cat_eve.setOnClickListener {
+            var intent= Intent(this,EventActivity::class.java)
+            startActivity(intent)
+
+        }
+        cat_ame.setOnClickListener {
+            var intent= Intent(this,AmenityActivity::class.java)
+            startActivity(intent)
+
+        }
+        cat_train.setOnClickListener {
+            var intent= Intent(this,RailwayActivity::class.java)
+            startActivity(intent)
+
+        }
+        cat_rep.setOnClickListener {
+            var intent= Intent(this,ReportActivity::class.java)
+            startActivity(intent)
+
+        }
+        qr.setOnClickListener {
+            var intent= Intent(this,QrcodeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        cat_pro.setOnClickListener {
+            var intent= Intent(this,PromtionActivity::class.java)
+            startActivity(intent)
+
+        }
     }}
