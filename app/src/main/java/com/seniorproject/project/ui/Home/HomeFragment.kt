f3aaafc14319e17a681a4bb7c63ac9c31ac9e977
Peerayos Lokwipas <paint.yb@gmail.com>
2021-05-22 16:39:45 +0700

    Show current weather condition and predict next5 hrs by using OpenWeatherAPI

--- app/src/main/java/com/seniorproject/project/ui/Home/HomeFragment.kt
+++ app/src/main/java/com/seniorproject/project/ui/Home/HomeFragment.kt
@@ -1,22 +1,31 @@
 package com.seniorproject.project.ui.Home
 
 import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.seniorproject.project.*
 import com.seniorproject.project.Adapters.FeedAdapter
-import kotlinx.android.synthetic.main.activity_attraction.*
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_profile.*
+import org.json.JSONObject
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private var SalayaLat = "13.800663"
+    private var SalayaLong = "100.323823"
+    private var API = "4282f657d89f45f71218e7bba5f90b1c"
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -32,6 +41,10 @@ class HomeFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
+
+        weatherTask().execute()
+
+
         all_cat.setOnClickListener {
             var intent= Intent(activity, Allcategories::class.java)
             startActivity(intent)
@@ -74,4 +87,105 @@ class HomeFragment : Fragment() {
         recycler.adapter = adapter
 
     }
+
+    inner class weatherTask(): AsyncTask<String, Void, String>() {
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+
+        override fun doInBackground(vararg params: String?): String? {
+            var response: String?
+            try {
+                response = URL("https://api.openweathermap.org/data/2.5/onecall?lat=$SalayaLat&lon=$SalayaLong&units=metric&exclude=alert,minutely,&appid=$API")
+                    .readText(Charsets.UTF_8)
+            }
+            catch (e: Exception){
+                response = null
+            }
+            return response
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            try {
+                val jsonObj = JSONObject(result)
+                val current = jsonObj.getJSONObject("current")
+                val weather = current.getJSONArray("weather").getJSONObject(0)
+                val picNow = weather.getString("icon")
+                Picasso.get().load("https://openweathermap.org/img/w/$picNow.png").into(icon)
+                val conditionNow = weather.getString("description")
+                val dateTime = current.getLong("dt")
+
+                val dateTimeText = SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH).format(Date(dateTime*1000))
+                val tempNow = current.getString("temp")+"°C"
+
+
+                val next1hr = jsonObj.getJSONArray("hourly").getJSONObject(0)
+                val weather1hr = next1hr.getJSONArray("weather").getJSONObject(0)
+                val pic1hr = weather1hr.getString("icon")
+                Picasso.get().load("https://openweathermap.org/img/w/$pic1hr.png").into(icon1)
+                val dateTime1 = next1hr.getLong("dt")
+                val dateTimeText1 = SimpleDateFormat("hh a", Locale.ENGLISH).format(Date(dateTime1*1000))
+                val tempNext1hr = next1hr.getString("temp")+"°C "
+
+                val next2hr = jsonObj.getJSONArray("hourly").getJSONObject(1)
+                val weather2hr = next2hr.getJSONArray("weather").getJSONObject(0)
+                val pic2hr = weather2hr.getString("icon")
+                Picasso.get().load("https://openweathermap.org/img/w/$pic2hr.png").into(icon2)
+                val dateTime2 = next2hr.getLong("dt")
+                val dateTimeText2 = SimpleDateFormat("hh a", Locale.ENGLISH).format(Date(dateTime2*1000))
+                val tempNext2hr = next2hr.getString("temp")+"°C "
+
+                val next3hr = jsonObj.getJSONArray("hourly").getJSONObject(2)
+                val weather3hr = next1hr.getJSONArray("weather").getJSONObject(0)
+                val pic3hr = weather1hr.getString("icon")
+                Picasso.get().load("https://openweathermap.org/img/w/$pic3hr.png").into(icon3)
+                val dateTime3 = next3hr.getLong("dt")
+                val dateTimeText3 = SimpleDateFormat("hh a", Locale.ENGLISH).format(Date(dateTime3*1000))
+                val tempNext3hr = next3hr.getString("temp")+"°C "
+
+                val next4hr = jsonObj.getJSONArray("hourly").getJSONObject(3)
+                val weather4hr = next4hr.getJSONArray("weather").getJSONObject(0)
+                val pic4hr = weather4hr.getString("icon")
+                Picasso.get().load("https://openweathermap.org/img/w/$pic4hr.png").into(icon4)
+                val dateTime4 = next4hr.getLong("dt")
+                val dateTimeText4 = SimpleDateFormat("hh a", Locale.ENGLISH).format(Date(dateTime4*1000))
+                val tempNext4hr = next4hr.getString("temp")+"°C "
+
+                val next5hr = jsonObj.getJSONArray("hourly").getJSONObject(4)
+                val weather5hr = next5hr.getJSONArray("weather").getJSONObject(0)
+                val pic5hr = weather5hr.getString("icon")
+                Picasso.get().load("https://openweathermap.org/img/w/$pic5hr.png").into(icon5)
+                val dateTime5 = next5hr.getLong("dt")
+                val dateTimeText5 = SimpleDateFormat("hh a", Locale.ENGLISH).format(Date(dateTime5*1000))
+                val tempNext5hr = next5hr.getString("temp")+"°C "
+
+                tempNowShow.text = tempNow
+                dateTimeShow.text = dateTimeText
+                conditionNowShow.text = conditionNow
+
+                next1tempShow.text = tempNext1hr
+                next1timeShow.text = dateTimeText1
+
+                next2tempShow.text = tempNext2hr
+                next2timeShow.text = dateTimeText2
+
+                next3tempShow.text = tempNext3hr
+                next3timeShow.text = dateTimeText3
+
+                next4tempShow.text = tempNext4hr
+                next4timeShow.text = dateTimeText4
+
+                next5tempShow.text = tempNext5hr
+                next5timeShow.text = dateTimeText5
+
+            }
+            catch (e: Exception){
+
+            }
+        }
+
+    }
+
 }
